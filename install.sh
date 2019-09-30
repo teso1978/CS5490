a4d89873c01a1319700c420de6c779d3476d90b0
crjens <crjens@hotmail.com>
2019-09-30 13:01:45 -0700

    fix version string

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo raspi-config nonint do_serial 2
 
 # Bluetooth uses the primary uart so it needs to be disabled
 # Only applies to RPi's with built-in Bluetooth (3A+, 3B, 3B+, 4B and Zero W)
-if grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F](08|0c|0d|0e|11)[0-9a-fA-F]$" /proc/cpuinfo'; then
+if grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F](08|0c|0d|0e|11)[0-9a-fA-F]$" /proc/cpuinfo; then
    echo '>>> Disable bluetooth'
    sudo raspi-config nonint set_config_var dtoverlay pi3-disable-bt /boot/config.txt
    sudo systemctl disable hciuart
